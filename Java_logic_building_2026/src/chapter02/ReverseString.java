bd4e23c4ab9e9c0a27f8655a526ba6e01e78ebe6
pranaypc <pranavghom028@gmail.com>
2026-08-14 00:26:15 +0530

    String Strated

---
+++ Java_logic_building_2026/src/chapter02/ReverseString.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+    String s = "java";
+   
+    for(int i = s.length()-1; i>=0;i--) {
+    System.out.print(s.charAt(i));	
+    }
+
+	}
+
+}
