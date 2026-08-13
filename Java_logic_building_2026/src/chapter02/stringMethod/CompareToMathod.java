89726a2f1b2b55ddb73251818904e525ed0a4d90
pranaypc <pranavghom028@gmail.com>
2026-08-13 14:16:02 +0530

    chapter 2 strated String

---
+++ Java_logic_building_2026/src/chapter02/stringMethod/CompareToMathod.java
@@ -0,0 +1,10 @@
+package chapter02.stringMethod;
+
+public class CompareToMathod {
+	public static void main(String args[]) {
+		String s1 = "Apple";
+		String s2 = "Banana";
+
+		System.out.println(s1.compareTo(s2));
+	}
+}
